b3e477b303c42ff003a4bf273f0cb9f80896e5b9
Linh Vu <thuylinh2490@gmail.com>
2019-10-03 17:18:13 +0700

    phan trang va tim kiem

---
+++ src/main/java/linhVu/service/CustomerService.java
@@ -0,0 +1,20 @@
+package linhVu.service;
+
+import linhVu.model.Customer;
+import linhVu.model.Province;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface CustomerService {
+    Page<Customer> findAll(Pageable pageable);
+
+    Customer findById(Long id);
+
+    void save(Customer customer);
+
+    void remove(Long id);
+
+    Iterable<Customer> findAllByProvince(Province province);
+
+    Page<Customer> findAllByFirstNameContaining(String firstName, Pageable pageable);
+}
